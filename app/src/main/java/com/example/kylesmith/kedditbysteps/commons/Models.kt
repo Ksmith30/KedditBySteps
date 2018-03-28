316213e189decfe812d1397293153a2b8c8692b6
Ksmith30 <kylesmith0630@gmail.com>
2018-03-28 09:59:26 -0500

    Add RedditNewsItem data class

---
+++ app/src/main/java/com/example/kylesmith/kedditbysteps/commons/Models.kt
@@ -0,0 +1,10 @@
+package com.example.kylesmith.kedditbysteps.commons
+
+data class RedditNewsItem (
+    val author: String,
+    val title: String,
+    val numComments: Int,
+    val created: Long,
+    val thumbnail: String,
+    val url: String
+)
