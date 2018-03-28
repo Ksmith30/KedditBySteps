9e25463268ebbf15e1c99162e25a12c381442d33
Ksmith30 <kylesmith0630@gmail.com>
2018-03-28 10:06:58 -0500

    Add LoadImage Function

--- app/src/main/java/com/example/kylesmith/kedditbysteps/commons/Models.kt
+++ app/src/main/java/com/example/kylesmith/kedditbysteps/commons/Models.kt
@@ -1,5 +1,8 @@
 package com.example.kylesmith.kedditbysteps.commons
 
+import com.example.kylesmith.kedditbysteps.commons.adapter.AdapterConstants
+import com.example.kylesmith.kedditbysteps.commons.adapter.ViewType
+
 data class RedditNewsItem (
     val author: String,
     val title: String,
@@ -7,4 +10,6 @@ data class RedditNewsItem (
     val created: Long,
     val thumbnail: String,
     val url: String
-)
+) : ViewType {
+    override fun getViewType() = AdapterConstants.NEWS
+}
